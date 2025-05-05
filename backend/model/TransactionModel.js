710ca0817ddded5719cdf0c97b232dbf3f005f02
Lasitha2001 <lasithamuthukumarana2022@gmail.com>
2025-05-05 22:38:51 +0530

    feat: simplify transaction model by removing unused fields

--- backend/model/TransactionModel.js
+++ backend/model/TransactionModel.js
@@ -4,16 +4,9 @@ const transactionSchema = new mongoose.Schema({
     items: [
         {
             _id: String,
-            name: String,
-            product_image: String,
-            category: String,
-            price: Number,
             quantity: Number,
         },
     ],
-    user_name: String,
-    email: String,
-    profile_image: String,
     total: Number,
     status: String,
     user_id: String,
